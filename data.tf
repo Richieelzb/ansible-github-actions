2b74323aeacbd4e70aae853508554920dd2895c0
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-01-08 14:49:32 +0200

    changing the ubuntu ami

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "my-data-ami" {
+data "aws_ami" "my-ubuntu-ami" {
   most_recent = true
   owners      = ["099720109477"]
 
@@ -12,6 +12,38 @@ data "aws_ami" "my-data-ami" {
     values = ["ebs"]
   }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+data "aws_ami" "my-centos-ami" {
+  most_recent = true
+  owners      = ["679593333241"]
+
+  filter {
+    name   = "name"
+    values = ["*-CentOs 9 x86_64 LATEST -*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+  
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
--- ec2-ansible-controller.tf
+++ ec2-ansible-controller.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ansible-ec2" {
-  ami = data.aws_ami.my-data-ami.id
+  ami = data.aws_ami.my-ubuntu-ami.id
   //ami                    = "ami-049442a6cf8319180"
   instance_type          = var.instance-type-list[0]
   key_name               = var.key-pair
