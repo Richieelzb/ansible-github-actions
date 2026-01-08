b4f8e1f0ed9d4322bc56ad30a156c5cb803bb071
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-01-08 14:17:03 +0200

    creating controller ec2

--- ec2-ansible-controller.tf
+++ ec2-ansible-controller.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ansible-ec2" {
   //ami = data.aws_ami.my-data-ami.id
-  ami = "ami-049442a6cf8319180"
+  ami                    = "ami-049442a6cf8319180"
   instance_type          = var.instance-type-list[0]
   key_name               = var.key-pair
   subnet_id              = module.vpc1.public_subnets[0]
