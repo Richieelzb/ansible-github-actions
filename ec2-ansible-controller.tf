5f7f3b5d10ffd250e11fad1177bbf0e2036c617d
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-01-08 14:06:38 +0200

    creating controller ec2

--- ec2-ansible-controller.tf
+++ ec2-ansible-controller.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "ansible-ec2" {
-  ami = data.aws_ami.my-data-ami.id
-  //ami = "ami-0bc691261a82b32bc"
-  instance_type = var.instance-type-list[0]
-  key_name      = var.key-pair
-  subnet_id     = module.vpc1.public_subnets[0]
-  user_data              = file("${path.module}/app-install.sh") 
+  //ami = data.aws_ami.my-data-ami.id
+  ami = "ami-049442a6cf8319180"
+  instance_type          = var.instance-type-list[0]
+  key_name               = var.key-pair
+  subnet_id              = module.vpc1.public_subnets[0]
+  user_data              = file("${path.module}/app-install.sh")
   vpc_security_group_ids = [aws_security_group.public-sg-vpc1.id]
   count                  = 1
   tags = {
