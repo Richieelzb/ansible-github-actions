76da39b99a70225ad082159faa053d589d69a68e
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-01-08 15:33:03 +0200

    deleting resources

--- ec2-public.tf
+++ ec2-public.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "ec2-webserver" {
   ami = data.aws_ami.my-amazon-ami.id
   //ami = "ami-0bc691261a82b32bc"
-  instance_type          = var.instance-type-list[0]
-  key_name               = var.key-pair
- // user_data              = file("${path.module}/app-install.sh")
+  instance_type = var.instance-type-list[0]
+  key_name      = var.key-pair
+  // user_data              = file("${path.module}/app-install.sh")
   subnet_id              = module.vpc1.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   #count                  = 3
@@ -15,9 +15,9 @@ resource "aws_instance" "ec2-webserver" {
 resource "aws_instance" "ec2-dbserver" {
   ami = data.aws_ami.my-amazon-ami.id
   //ami = "ami-0bc691261a82b32bc"
-  instance_type          = var.instance-type-list[0]
-  key_name               = var.key-pair
- // user_data              = file("${path.module}/app-install.sh")
+  instance_type = var.instance-type-list[0]
+  key_name      = var.key-pair
+  // user_data              = file("${path.module}/app-install.sh")
   subnet_id              = module.vpc1.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   #count                  = 3
